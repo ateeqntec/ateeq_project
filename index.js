07bfaa3cf874a7a840be37ff2bc6f45a372ed38d
ateeqntec <ateeqntech@gmail.com>
2022-09-08 18:24:00 +0500

    'projectstarted'

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World ateeq')
+  res.send('Hello World')
 })
 
-app.listen(3001)
+app.listen(3002)
